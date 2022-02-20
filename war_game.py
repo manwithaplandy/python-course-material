75d1bbb9523af62075350b9399f0b7dcf53d1b15
manwithaplandy <abysmalduck@gmail.com>
2022-02-20 12:50:39 -0800

    Built game logic before video explanation

--- war_game.py
+++ war_game.py
@@ -1,4 +1,4 @@
-'''
+"""
 Card game of War.
 
 Rules are simple - two players split a deck of cards. Each turn, both players will draw the top card from their deck.
@@ -7,7 +7,7 @@ If the card values are tied, it kicks off a "war", where both players will place
 the top of the deck, and show a fourth. Who ever wins based on the value of the fourth card wins all cards placed
 on the table that round.
 The winner is determined when one player runs out of cards.
-'''
+"""
 
 import random
 
@@ -64,11 +64,83 @@ class Player:
 
     def add_cards(self, new_cards):
         if type(new_cards) == type([]):
+            # use extend method if multiple cards being added
             self.all_cards.extend(new_cards)
         else:
             self.all_cards.append(new_cards)
 
     def __str__(self):
         return f'Player {self.name} has {len(self.all_cards)} cards.'
+
+
+# Game set-up
+
+player_one = Player('One')
+player_two = Player('Two')
+round_number = 0
+
+main_deck = Deck()
+main_deck.shuffle()
+
+for i in range(26):
+    player_one.add_cards(main_deck.deal_one())
+    player_two.add_cards(main_deck.deal_one())
+
+game_on = True
+at_war = False
+
+
+def check_win():
+    global game_on
+    if len(player_one.all_cards) == 0:
+        game_on = False
+        print('Player Two wins!')
+    elif len(player_two.all_cards) == 0:
+        game_on = False
+        print('Player One wins!')
+
+
+while game_on:
+
+    global game_on
+    global at_war
+    # Count the number of rounds
+    round_number += 1
+    print(f'Current round number: {round_number}')
+
+    # Check if a player has lost
+    check_win()
+
+    # Start a new round
+    player_one_cards = []
+    player_one_cards.append(player_one.remove_one())
+
+    player_two_cards = []
+    player_one_cards.append(player_one.remove_one())
+
+    table_cards = [player_two_cards[0], player_one_cards[0]]
+
+    if player_two_cards[0] > player_one_cards[0]:
+        player_two.add_cards(table_cards)
+    elif player_one_cards[0] > player_two_cards[0]:
+        player_one.add_cards(table_cards)
+    else:
+        at_war = True
+
+    while at_war:
+        table_cards.extend([player_one.remove_one(), player_one.remove_one(), player_one.remove_one(), player_two.remove_one(), player_two.remove_one(), player_two.remove_one()])
+        player_one_cards = [player_one.remove_one()]
+        player_two_cards = [player_two.remove_one()]
+        if player_two_cards[0] > player_one_cards[0]:
+            player_two.add_cards(table_cards)
+            at_war = False
+        elif player_one_cards[0] > player_two_cards[0]:
+            player_one.add_cards(table_cards)
+            at_war = False
+        else:
+            continue
+
+
+
 
 
